e99478d278e9212435b2d5dc98230c9bd33f7206
林子佑 <42837758+linziyou0601@users.noreply.github.com>
2019-07-28 02:44:36 +0800

    lzy

---
+++ Taipei_MRT_BlueLine/README.md
@@ -0,0 +1,8 @@
+# 北捷板南線2048 | Taipei MRT Blue Line 2048
+
+https://linziyou0601.github.io/2048Remake_by_Linziyou/Taipei_MRT_BlueLine/index.html
+
+## How To Play
+就，玩阿ww
+
+> Fork From https://github.com/gabrielecirulli/2048/
---
+++ Taipei_MRT_BlueLine/js/html_actuator.js
@@ -0,0 +1,168 @@
+function HTMLActuator() {
+  this.tileContainer    = document.querySelector(".tile-container");
+  this.scoreContainer   = document.querySelector(".score-container");
+  this.bestContainer    = document.querySelector(".best-container");
+  this.messageContainer = document.querySelector(".game-message");
+
+  this.score = 0;
+}
+
+HTMLActuator.prototype.actuate = function (grid, metadata) {
+  var self = this;
+
+  window.requestAnimationFrame(function () {
+    self.clearContainer(self.tileContainer);
+
+    grid.cells.forEach(function (column) {
+      column.forEach(function (cell) {
+        if (cell) {
+          self.addTile(cell);
+        }
+      });
+    });
+
+    self.updateScore(metadata.score);
+    self.updateBestScore(metadata.bestScore);
+
+    if (metadata.terminated) {
+      if (metadata.over) {
+        self.message(false); // You lose
+      } else if (metadata.won) {
+        self.message(true); // You win!
+      }
+    }
+
+  });
+};
+
+// Continues the game (both restart and keep playing)
+HTMLActuator.prototype.continueGame = function () {
+  this.clearMessage();
+};
+
+HTMLActuator.prototype.clearContainer = function (container) {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+};
+
+HTMLActuator.prototype.addTile = function (tile) {
+  var self = this;
+
+  var wrapper   = document.createElement("div");
+  var inner     = document.createElement("div");
+  var position  = tile.previousPosition || { x: tile.x, y: tile.y };
+  var positionClass = this.positionClass(position);
+
+  // We can't use classlist because it somehow glitches when replacing classes
+  var classes = ["tile", "tile-" + tile.value, positionClass];
+
+  if (tile.value > 8388608) classes.push("tile-super");
+
+  this.applyClasses(wrapper, classes);
+  
+  const mapping = {
+    2:"頂埔",
+    4:"永寧",
+    8:"土城",
+    16:"海山",
+    32:"亞東醫院",
+    64:"府中",
+    128:"板橋",
+    256:"新埔",
+    512:"江子翠",
+    1024:"龍山寺",
+    2048:"西門",
+    4096:"台北車站",
+    8192:"善導寺",
+    16384:"忠孝新生",
+    32768:"忠孝復興",
+    65536:"忠孝敦化",
+    131072:"國父紀念館",
+    262144:"市政府",
+    524288:"永春",
+    1048576:"後山埤",
+    2097152:"昆陽",
+    4194304:"南港",
+    8388608:"南港展覽館"
+  }
+
+  inner.classList.add("tile-inner");
+  if (tile.value <= 8388608)
+    inner.textContent = mapping[tile.value];
+  else
+    inner.textContent = "板南王 Lv." + (Math.log2(tile.value) - 23);
+
+  if (tile.previousPosition) {
+    // Make sure that the tile gets rendered in the previous position first
+    window.requestAnimationFrame(function () {
+      classes[2] = self.positionClass({ x: tile.x, y: tile.y });
+      self.applyClasses(wrapper, classes); // Update the position
+    });
+  } else if (tile.mergedFrom) {
+    classes.push("tile-merged");
+    this.applyClasses(wrapper, classes);
+
+    // Render the tiles that merged
+    tile.mergedFrom.forEach(function (merged) {
+      self.addTile(merged);
+    });
+  } else {
+    classes.push("tile-new");
+    this.applyClasses(wrapper, classes);
+  }
+
+  // Add the inner part of the tile to the wrapper
+  wrapper.appendChild(inner);
+
+  // Put the tile on the board
+  this.tileContainer.appendChild(wrapper);
+};
+
+HTMLActuator.prototype.applyClasses = function (element, classes) {
+  element.setAttribute("class", classes.join(" "));
+};
+
+HTMLActuator.prototype.normalizePosition = function (position) {
+  return { x: position.x + 1, y: position.y + 1 };
+};
+
+HTMLActuator.prototype.positionClass = function (position) {
+  position = this.normalizePosition(position);
+  return "tile-position-" + position.x + "-" + position.y;
+};
+
+HTMLActuator.prototype.updateScore = function (score) {
+  this.clearContainer(this.scoreContainer);
+
+  var difference = score - this.score;
+  this.score = score;
+
+  this.scoreContainer.textContent = this.score;
+
+  if (difference > 0) {
+    var addition = document.createElement("div");
+    addition.classList.add("score-addition");
+    addition.textContent = "+" + difference;
+
+    this.scoreContainer.appendChild(addition);
+  }
+};
+
+HTMLActuator.prototype.updateBestScore = function (bestScore) {
+  this.bestContainer.textContent = bestScore;
+};
+
+HTMLActuator.prototype.message = function (won) {
+  var type    = won ? "game-won" : "game-over";
+  var message = won ? "抵達人最多的站！" : "請重新搭乘！";
+
+  this.messageContainer.classList.add(type);
+  this.messageContainer.getElementsByTagName("p")[0].textContent = message;
+};
+
+HTMLActuator.prototype.clearMessage = function () {
+  // IE only takes one value to remove at a time.
+  this.messageContainer.classList.remove("game-won");
+  this.messageContainer.classList.remove("game-over");
+};
